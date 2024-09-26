fbdf3926d4a1f67bc6f98501e858398ee649e26c
martinjeromelouis <32217468+martinjeromelouis@users.noreply.github.com>
2024-09-26 15:00:55 +0200

    chore: rebuilt pretty-print

--- packages/pretty-print/project.config.js
+++ packages/pretty-print/project.config.js
@@ -20,7 +20,7 @@ const packageJson = merge.all([
 		publishConfig: {
 			peerDependencies: {
 				[`${Configs.constants.scope}/js-lib`]: '^0.0.7',
-				[`${Configs.constants.scope}/effect-lib`]: '^0.3.2'
+				[`${Configs.constants.scope}/effect-lib`]: '^0.3.3'
 			}
 		},
 		scripts: {
