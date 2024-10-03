983ba5c677d658f18b16ada69aeff6361e642381
martinjeromelouis <32217468+martinjeromelouis@users.noreply.github.com>
2024-10-03 10:54:23 +0200

    feat: created playground package

---
+++ packages/playground/project.config.js
@@ -0,0 +1,44 @@
+import * as Configs from '@parischap/configs';
+import merge from 'deepmerge';
+import { basename, dirname, resolve } from 'node:path';
+
+const rootPath = resolve();
+const packageName = basename(rootPath);
+const repoName = basename(dirname(dirname(rootPath)));
+
+const packageJson = merge.all([
+	Configs.packageBase(packageName, repoName),
+	Configs.packageSubRepo,
+	Configs.packageSubRepoBundled,
+	Configs.packageSubRepoBundledEffect,
+	Configs.packageSubRepoPrivate,
+	{
+		description: 'Playground',
+		dependencies: {
+			'@parischap/js-lib': '^0.0.7',
+			'@parischap/effect-lib': '^0.3.3',
+			effect: '^3.8.4',
+			'@parischap/pretty-print': '^0.2.0'
+		},
+		scripts: {
+			play: 'vite-node esm/index.ts'
+		}
+	}
+]);
+
+export default {
+	// Put prettier in first position so the next generated files will get formatted
+	[Configs.constants.prettierConfigFileName]: Configs.prettierConfigTemplate,
+	[Configs.constants.gitIgnoreFileName]: Configs.gitIgnoreTemplate,
+	[Configs.constants.madgeConfigFileName]: Configs.madgercTemplate,
+	[Configs.constants.prettierIgnoreFileName]: Configs.prettierIgnore,
+	[Configs.constants.baseTsConfigFileName]: Configs.tsConfigBase,
+	[Configs.constants.projectTsConfigFileName]: Configs.tsConfigSrcLibrary,
+	[Configs.constants.nonProjectTsConfigFileName]: Configs.tsConfigOthers,
+	[Configs.constants.tsConfigFileName]: Configs.tsConfig,
+	[Configs.constants.tscLintTsConfigFileName]: Configs.tsConfigCheck,
+	[Configs.constants.licenseFileName]: Configs.licenseTemplate,
+	[Configs.constants.eslintConfigFileName]: Configs.eslintConfigLibraryTemplate,
+	[Configs.constants.viteConfigFileName]: 'export default {};',
+	[Configs.constants.packageJsonFileName]: packageJson
+};
