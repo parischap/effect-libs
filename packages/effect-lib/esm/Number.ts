0577b8063ec5407720f25a6b1ddf664c2c4d1237
martinjeromelouis <32217468+martinjeromelouis@users.noreply.github.com>
2024-10-03 11:22:45 +0200

    fix: corrected bug in MNumber isFinite, isNotFinite, isInt, isNotInt

--- packages/effect-lib/esm/Number.ts
+++ packages/effect-lib/esm/Number.ts
@@ -5,7 +5,6 @@
  */
 
 import { JsPatches } from '@parischap/js-lib';
-import { Predicate } from 'effect';
 
 /**
  * Constructs an integer from a string in base 10. Does not check input format and can return NaN
@@ -17,7 +16,8 @@ import { Predicate } from 'effect';
 export const unsafeIntFromString = (s: string): number => parseInt(s, 10);
 
 /**
- * Constructs a number from a string. Does not check input format and can return NaN
+ * Constructs a number from a string. Does not check input format and can return NaN, 'Infinity',
+ * '+Infinity', '-Infinity'
  *
  * @since 0.0.6
  * @category Constructors
@@ -35,19 +35,37 @@ export const unsafeFromString = (s: string): number => +s;
 export const intModulo = JsPatches.intModulo;
 
 /**
- * Returns true if the provided number is NaN, +Infinity or -Infinity
+ * Returns true if the provided number is NaN, Infinity, +Infinity or -Infinity
  *
  * @since 0.3.4
  * @category Utils
  */
-
-export const isNotFinite = Predicate.not(Number.isFinite);
+// DO NOT USE A TACIT FUNCTION because Number.isFinite takes an unknwon parameter
+export const isNotFinite = (n: number) => !Number.isFinite(n);
 
 /**
- * Returns true if the provided number is not NaN, +Infinity or -Infinity
+ * Returns true if the provided number is not NaN, Infinity, +Infinity or -Infinity
  *
  * @since 0.3.4
  * @category Utils
  */
+// DO NOT USE A TACIT FUNCTION because Number.isFinite takes an unknwon parameter
+export const isFinite = (n: number) => Number.isFinite(n);
 
-export const isFinite = Number.isFinite;
+/**
+ * Returns true if the provided number is an integer
+ *
+ * @since 0.3.4
+ * @category Utils
+ */
+// DO NOT USE A TACIT FUNCTION because Number.isInteger takes an unknwon parameter
+export const isInt = (n: number) => Number.isInteger(n);
+
+/**
+ * Returns true if the provided number is not an integer
+ *
+ * @since 0.3.4
+ * @category Utils
+ */
+// DO NOT USE A TACIT FUNCTION because Number.isInteger takes an unknwon parameter
+export const isNotInt = (n: number) => !Number.isInteger(n);
