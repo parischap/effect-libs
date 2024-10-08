dcc66f10b56e5f68a3ab51304e464f2be367f1e4
martinjeromelouis <32217468+martinjeromelouis@users.noreply.github.com>
2024-10-08 11:36:26 +0200

    chore: made PortError tag more unique

--- packages/effect-lib/esm/PortError.ts
+++ packages/effect-lib/esm/PortError.ts
@@ -6,13 +6,15 @@
 
 import { Data } from 'effect';
 
+export const moduleTag = '@parischap/effect-lib/PortError/';
+
 /**
  * FunctionPort signals an error that occurs while Effectifying a function
  *
  * @since 0.0.6
  * @category Models
  */
-export class Type extends Data.TaggedError('FunctionPort')<{
+export class Type extends Data.TaggedError(moduleTag)<{
 	readonly originalError: unknown;
 	readonly originalFunctionName: string;
 	readonly moduleName: string;
