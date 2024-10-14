ba5b3ee085086cd2f112486e679a4daa90ab775d
martinjeromelouis <32217468+martinjeromelouis@users.noreply.github.com>
2024-10-14 16:11:03 +0200

    Feat: added constEmptyString function

--- packages/effect-lib/esm/Function.ts
+++ packages/effect-lib/esm/Function.ts
@@ -112,3 +112,11 @@ export const applyAsMethod =
 	(o: MTypes.AnyRecord) =>
 	<A>(self: LazyArg<A>): A =>
 		self.call(o);
+
+/**
+ * A function that returns an empty string
+ *
+ * @since 0.2.2
+ * @category Utils
+ */
+export const constEmptyString = () => '';
