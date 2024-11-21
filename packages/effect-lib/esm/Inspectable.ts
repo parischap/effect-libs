0b675ec30972c0f14a60d12d527df56e39fed19f
martinjeromelouis <32217468+martinjeromelouis@users.noreply.github.com>
2024-11-21 17:44:14 +0100

    feat: introduced NameSymbol property which displays object as a name when present

--- packages/effect-lib/esm/Inspectable.ts
+++ packages/effect-lib/esm/Inspectable.ts
@@ -4,17 +4,45 @@
  * @since 0.3.3
  */
 
-import { Inspectable } from 'effect';
+import { Inspectable as EInspectable } from 'effect';
+import * as MTypes from './types.js';
+
+const moduleTag = '@parischap/effect-lib/Inspectable/';
 
 /**
- * Default prototype of an inspectable that adds the `toJSON` method
+ * Symbol used to define a special prototype function that must return a name identifying the object
+ *
+ * @since 0.5.0
+ * @category Models
+ */
+export const NameSymbol: unique symbol = Symbol.for(moduleTag + 'NameSymbol/') as NameSymbol;
+
+/**
+ * Type used to define a special prototype function that must return a name identifying the object
+ *
+ * @since 0.5.0
+ * @category Models
+ */
+export type NameSymbol = typeof NameSymbol;
+
+export interface Inspectable extends EInspectable.Inspectable {
+	readonly [NameSymbol]: () => string;
+}
+/**
+ * Prototype of an `Inspectable` that overloads the `toJSON` method. If the object (usually its
+ * prototype) has a `[NameSymbol]` function, returns the result of this function. Otherwise, return
+ * this with an extra '_id' field containing the moduleTag.
  *
  * @since 0.3.3
  * @category Instances
  */
-export const BaseProto = (moduleTag: string): Inspectable.Inspectable => ({
-	...Inspectable.BaseProto,
+export const BaseProto = (moduleTag: string): EInspectable.Inspectable => ({
+	...EInspectable.BaseProto,
 	toJSON(this: {}) {
-		return { _id: () => moduleTag, ...this };
+		return (
+				NameSymbol in this && MTypes.isFunction(this[NameSymbol]) && this[NameSymbol].length === 0
+			) ?
+				(this[NameSymbol]() as unknown)
+			:	{ _id: moduleTag, ...this };
 	}
 });
