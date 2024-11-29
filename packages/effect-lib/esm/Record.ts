b40bc19aa45f6963d88fd5529c805673accd5f4f
martinjeromelouis <32217468+martinjeromelouis@users.noreply.github.com>
2024-11-29 17:57:32 +0100

    feat: improved unsafeGet and added tryZeroParamStringFunction

--- packages/effect-lib/esm/Record.ts
+++ packages/effect-lib/esm/Record.ts
@@ -4,7 +4,9 @@
  * @since 0.0.6
  */
 
-import { Record } from 'effect';
+import { flow, Option, pipe, Predicate, Record } from 'effect';
+import * as MFunction from './Function.js';
+import * as MTypes from './types.js';
 
 /**
  * Unsafe get an element from a record. No checks, faster than the Effect version
@@ -13,7 +15,37 @@ import { Record } from 'effect';
  * @category Utils
  */
 export const unsafeGet =
-	(key: string) =>
+	(key: string | symbol) =>
 	<A>(self: Record.ReadonlyRecord<string, A>): A =>
 		// @ts-expect-error getting record content unsafely
 		self[key];
+
+/**
+ * Tries to call method `functionName` on `self` without any parameters. Returns a `some` of the
+ * result if such a function exists and returns a string. Returns a `none` otherwise
+ *
+ * @since 0.0.6
+ * @category Utils
+ */
+export const tryZeroParamStringFunction =
+	({
+		functionName,
+		exception
+	}: {
+		readonly functionName: string | symbol;
+		readonly exception?: MTypes.AnyFunction;
+	}) =>
+	(self: MTypes.AnyRecord): Option.Option<string> =>
+		pipe(
+			self,
+			Record.get(functionName),
+			Option.filter(MTypes.isFunction),
+			Option.filter(
+				Predicate.and(
+					Predicate.not(MFunction.strictEquals(exception)),
+					flow(MFunction.parameterNumber, MFunction.strictEquals(0))
+				)
+			),
+			Option.map(MFunction.applyAsMethod(self)),
+			Option.filter(MTypes.isString)
+		);
