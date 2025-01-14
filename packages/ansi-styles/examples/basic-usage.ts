d0edb3cd4d9beffe68e9b7f6a65d04f996bfb15a
martinjeromelouis <32217468+martinjeromelouis@users.noreply.github.com>
2025-01-14 14:15:42 +0100

    chore: started adding examples

---
+++ packages/ansi-styles/examples/basic-usage.ts
@@ -0,0 +1,17 @@
+/* eslint-disable functional/no-expression-statements */
+import { ASStyle } from '@parischap/ansi-styles';
+
+console.log(
+	ASStyle.red(
+		'ansi-styles is an ',
+		ASStyle.bold(
+			'Effect library ',
+			ASStyle.magenta(
+				ASStyle.dim('for terminal output styling with '),
+				ASStyle.yellow('ANSI '),
+				'colors '
+			)
+		),
+		'and formats.'
+	)
+);
