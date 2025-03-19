421ab25611cca3bc368ff0c0fa082ffbb890b8c2
martinjeromelouis <32217468+martinjeromelouis@users.noreply.github.com>
2025-03-19 18:13:23 +0100

    feat: improved matchAndGroups, added capturedGroups

--- packages/effect-lib/esm/RegExp.ts
+++ packages/effect-lib/esm/RegExp.ts
@@ -34,25 +34,41 @@ export const match =
  * @category Destructors
  */
 export const matchAndGroups =
-	(s: string) =>
-	(self: RegExp): Option.Option<[match: string, capturingGroups: Array<string>]> =>
+	<N extends number>(s: string, capturingGroupNumber: N) =>
+	(self: RegExp): Option.Option<[match: string, capturingGroups: MTypes.Tuple<string, N>]> =>
 		pipe(
 			self.exec(s),
 			Option.fromNullable,
 			// RegExpExecArray extends from Array<string>. But this is a Typescript bug. When there are optional capturing groups, there can be some undefined elements. So let's make javascript and Typescript coherent.
 			Option.map(
 				flow(
-					Array.map(
-						flow(
-							Option.liftPredicate(MTypes.isNotUndefined),
-							Option.getOrElse(MFunction.constEmptyString)
-						)
-					),
 					Array.splitAt(1),
-					Tuple.mapFirst(MArray.unsafeGet(0))
+					Tuple.mapBoth({
+						onFirst: MArray.unsafeGet(0),
+						onSecond: flow(
+							Array.pad(capturingGroupNumber, ''),
+							Array.map(
+								flow(
+									Option.liftPredicate(MTypes.isNotUndefined),
+									Option.getOrElse(MFunction.constEmptyString)
+								)
+							)
+						)
+					})
 				)
 			)
-		);
+		) as never;
+
+/**
+ * Same as matchAndGroups but returns only the captured groups.
+ *
+ * @category Destructors
+ */
+export const capturedGroups = <N extends number>(
+	s: string,
+	capturingGroupNumber: N
+): MTypes.OneArgFunction<RegExp, Option.Option<MTypes.Tuple<string, N>>> =>
+	flow(matchAndGroups(s, capturingGroupNumber), Option.map(Tuple.getSecond));
 
 /**
  * A regular expression representing a linebreak in all systems with the `g` flag. ATTENTION: MUST
@@ -61,39 +77,39 @@ export const matchAndGroups =
  *
  * @category Instances
  */
-export const globalLineBreak = new RegExp(MRegExpString.lineBreak, 'g');
+export const globalLineBreak: RegExp = new RegExp(MRegExpString.lineBreak, 'g');
 
 /**
  * A regular expression representing a linebreak in all systems without the `g` flag.
  *
  * @category Instances
  */
-export const lineBreak = new RegExp(MRegExpString.lineBreak);
+export const lineBreak: RegExp = new RegExp(MRegExpString.lineBreak);
 
 /**
  * A regular expression representing a path separator that will match on all systems
  *
  * @category Instances
  */
-export const universalPathSep = new RegExp(MRegExpString.universalPathSep);
+export const universalPathSep: RegExp = new RegExp(MRegExpString.universalPathSep);
 
 /**
  * A regular expression representing a SemVer
  *
  * @category Instances
  */
-export const semVer = pipe(MRegExpString.semVer, MRegExpString.makeLine, fromRegExpString());
+export const semVer: RegExp = pipe(MRegExpString.semVer, MRegExpString.makeLine, RegExp);
 
 /**
  * A regular expression representing an email
  *
  * @category Instances
  */
-export const email = pipe(MRegExpString.email, MRegExpString.makeLine, fromRegExpString());
+export const email: RegExp = pipe(MRegExpString.email, MRegExpString.makeLine, RegExp);
 
 /**
  * A regular expression representing a strictly positive digit
  *
  * @category Instances
  */
-export const nonZeroDigit = pipe(MRegExpString.nonZeroDigit, fromRegExpString());
+export const nonZeroDigit: RegExp = pipe(MRegExpString.nonZeroDigit, RegExp);
--- packages/effect-lib/tests/RegExp.test.ts
+++ packages/effect-lib/tests/RegExp.test.ts
@@ -3,31 +3,43 @@ import { MRegExp } from '@parischap/effect-lib';
 import { Array, Option, pipe, String } from 'effect';
 import { describe, expect, it } from 'vitest';
 
-describe('match', () => {
-	const stringOptionEq = Option.getEquivalence(String.Equivalence);
-	const regExp = /a.+a/;
-	it('Matching', () => {
-		expect(stringOptionEq(pipe(regExp, MRegExp.match('afooa')), Option.some('afooa'))).toBe(true);
+const stringArrayEq = Array.getEquivalence(String.Equivalence);
+const regExp = /af(o)o(bar)?a/;
+
+describe('MRegExp', () => {
+	describe('match', () => {
+		const stringOptionEq = Option.getEquivalence(String.Equivalence);
+		it('Matching', () => {
+			expect(stringOptionEq(pipe(regExp, MRegExp.match('afooa')), Option.some('afooa'))).toBe(true);
+			expect(stringOptionEq(pipe(regExp, MRegExp.match('afoobara')), Option.some('afoobara'))).toBe(
+				true
+			);
+		});
+
+		it('Non matching', () => {
+			expect(pipe(regExp, MRegExp.match('afoob'), Option.isNone)).toBe(true);
+		});
 	});
 
-	it('Non matching', () => {
-		expect(pipe(regExp, MRegExp.match('afoob'), Option.isNone)).toBe(true);
-	});
-});
+	describe('matchAndGroups', () => {
+		it('Matching', () => {
+			const result = pipe(regExp, MRegExp.matchAndGroups('afooa', 2));
+			expect(
+				Option.isSome(result) &&
+					result.value[0] === 'afooa' &&
+					stringArrayEq(result.value[1], Array.make('o', ''))
+			).toBe(true);
+		});
 
-describe('matchAndGroups', () => {
-	const stringArrayEq = Array.getEquivalence(String.Equivalence);
-	const regExp = /af(o)o(bar)?a/;
-	it('Matching', () => {
-		const result = pipe(regExp, MRegExp.matchAndGroups('afooa'));
-		expect(
-			Option.isSome(result) &&
-				result.value[0] === 'afooa' &&
-				stringArrayEq(result.value[1], Array.make('o', ''))
-		).toBe(true);
+		it('Non matching', () => {
+			expect(pipe(regExp, MRegExp.match('afoob'), Option.isNone)).toBe(true);
+		});
 	});
 
-	it('Non matching', () => {
-		expect(pipe(regExp, MRegExp.match('afoob'), Option.isNone)).toBe(true);
+	describe('capturedGroups', () => {
+		it('Matching', () => {
+			const result = pipe(regExp, MRegExp.capturedGroups('afooa', 2));
+			expect(Option.isSome(result) && stringArrayEq(result.value, Array.make('o', ''))).toBe(true);
+		});
 	});
 });
