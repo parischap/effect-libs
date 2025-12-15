9c6123b799435e4f7a302aca61aa72bf42dfda7b
martinjeromelouis <32217468+martinjeromelouis@users.noreply.github.com>
2025-12-15 17:55:03 +0100

    On-going dev

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,9 +1,4 @@
 // ************** This file was automatically generated. DO NOT MODIFY ************************
 
-import { defineConfig } from 'vitest/config'
-
-export default defineConfig({
-  test: {
-    projects: ['packages/*'],
-  },
-})
+import {config} from '@parischap/configs/VitestConfigNoSource';
+export default config
