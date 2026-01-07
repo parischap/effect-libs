6e134122d880f28aae36610892e61e00bf651da1
martinjeromelouis <32217468+martinjeromelouis@users.noreply.github.com>
2026-01-07 18:19:15 +0100

    On-going dev

--- packages/node-effect-lib/eslint.config.ts
+++ packages/node-effect-lib/eslint.config.ts
@@ -1,17 +1,2 @@
-import { eslintStyleIncludeForSourceFiles } from '../../../shared-utils/constants.js';
-
-//import { importX } from 'eslint-plugin-import-x';
-import { defineConfig, type Config } from 'eslint/config';
-import globals from 'globals';
-import EslintConfigBase from './eslint.config.base.template.js';
-
-export default defineConfig([
-  ...EslintConfigBase,
-  {
-    files: eslintStyleIncludeForSourceFiles,
-    name: 'javascriptConfigSourceFiles',
-    languageOptions: {
-      globals: globals.nodeBuiltin,
-    },
-  },
-]) satisfies Array<Config>;
+import Config from '@parischap/configs/EslintConfigPlain';
+export default Config({tsconfigRootDir:import.meta.dirname})
--- vitest.config.ts
+++ vitest.config.ts
@@ -1,9 +1,2 @@
-/* Although it is possible to have the vitest configuration inside the vite configuration files, this is usually not a good idea. The vite configuration is useful for bundling whereas the vitest configuration is useful for testing */
-import { defineConfig } from 'vitest/config';
-import { packagesFolderName } from '../../../shared-utils/constants.js';
-
-export default defineConfig({
-  test: {
-    projects: [`${packagesFolderName}/*`],
-  },
-});
+import Config from '@parischap/configs/VitestConfigNoSource';
+export default Config
