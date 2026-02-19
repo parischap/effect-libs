b0793aa63660570b11b996d2d66e0e84c9c76cc5
martinjeromelouis <32217468+martinjeromelouis@users.noreply.github.com>
2026-02-19 17:55:21 +0100

    fix: removed forgottent console.log

--- packages/effect-lib/tests/Tree/TreeLeaf.test.ts
+++ packages/effect-lib/tests/Tree/TreeLeaf.test.ts
@@ -1,12 +1,11 @@
 import * as TestUtils from '@parischap/configs/TestUtils';
-import * as MTreeLeaf from '@parischap/effect-lib/MTreeLeaf'
-import * as Option from 'effect/Option'
+import * as MTreeLeaf from '@parischap/effect-lib/MTreeLeaf';
+import * as Option from 'effect/Option';
 import { describe, it } from 'vitest';
 
 describe('MTreeLeaf', () => {
   describe('Tag', () => {
     it('moduleTag', () => {
-      console.log(TestUtils.moduleTagFromTestFilePath(import.meta.filename));
       TestUtils.assertEquals(
         Option.some(MTreeLeaf.moduleTag),
         TestUtils.moduleTagFromTestFilePath(import.meta.filename),
