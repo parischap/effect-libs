e2406757f1e9ec8b8eb844d171cec0964e4cf756
parischap <jerome.martin.pro@gmail.com>
2026-06-16 10:18:58 +0200

    chore: added rounding folders again

---
+++ packages/conversions/esm/rounding/Rounder.ts
@@ -0,0 +1,85 @@
+/**
+ * This module implements a CVRounder, i.e. a function that can round a number or a BigDecimal to
+ * the given `precision` and according to the passed `CVRoundingOption`
+ */
+
+import { pipe } from 'effect';
+import * as BigDecimal from 'effect/BigDecimal';
+
+import * as MBigDecimal from '@parischap/effect-lib/MBigDecimal';
+import * as MBigInt from '@parischap/effect-lib/MBigInt';
+import * as MNumber from '@parischap/effect-lib/MNumber';
+import type * as MTypes from '@parischap/effect-lib/MTypes';
+
+import type * as CVRounderParams from './RounderParams.js';
+
+import * as CVRoundingOptionCorrecter from '../internal/rounding/RoundingOption/RoundingOptionCorrecter.js';
+
+const bigDecimal10 = BigDecimal.make(10n, 0);
+
+/**
+ * Type of a CVRounder
+ *
+ * @category Models
+ */
+export interface Type<
+  in out N extends number | BigDecimal.BigDecimal,
+> extends MTypes.OneArgFunction<N> {}
+
+/**
+ * Constructor of a number rounder from a CVRounderParams
+ *
+ * @category Constructors
+ */
+export const number = (rounderParams: CVRounderParams.Type): Type<number> => {
+  const shiftMultiplicand = pipe(1, MNumber.shift(rounderParams.precision));
+  const unshiftMultiplicand = 1 / shiftMultiplicand;
+
+  const correcter = CVRoundingOptionCorrecter.fromRoundingOption(rounderParams.roundingOption);
+
+  return (n) => {
+    const shiftedSelf = shiftMultiplicand * n;
+    const truncatedShiftedSelf = Math.trunc(shiftedSelf);
+    const firstFollowingDigit = Math.trunc((shiftedSelf - truncatedShiftedSelf) * 10);
+    return (
+      unshiftMultiplicand *
+      (truncatedShiftedSelf +
+        correcter({ firstFollowingDigit, isEven: truncatedShiftedSelf % 2 === 0 }))
+    );
+  };
+};
+
+/**
+ * Constructor of a BigDecimal rounder from a CVRounderParams
+ *
+ * @category Constructors
+ */
+export const bigDecimal = (rounderParams: CVRounderParams.Type): Type<BigDecimal.BigDecimal> => {
+  const shiftValue = BigDecimal.make(1n, -rounderParams.precision);
+  const shift = BigDecimal.multiply(shiftValue);
+  const unshift = BigDecimal.divideUnsafe(shiftValue);
+  const correcter = CVRoundingOptionCorrecter.fromRoundingOption(rounderParams.roundingOption);
+
+  return (n) => {
+    const shiftedSelf = shift(n);
+    const truncatedShiftedSelf = pipe(shiftedSelf, MBigDecimal.trunc());
+    const firstFollowingDigit = pipe(
+      shiftedSelf,
+      BigDecimal.subtract(truncatedShiftedSelf),
+      BigDecimal.multiply(bigDecimal10),
+      MBigDecimal.trunc(),
+      BigDecimal.toNumberUnsafe,
+    );
+    return pipe(
+      truncatedShiftedSelf,
+      BigDecimal.sum(
+        pipe(
+          { firstFollowingDigit, isEven: MBigInt.isEven(truncatedShiftedSelf.value) },
+          correcter,
+          BigDecimal.fromNumberUnsafe,
+        ),
+      ),
+      unshift,
+    );
+  };
+};
---
+++ packages/conversions/tests/rounding/RounderParams.test.ts
@@ -0,0 +1,26 @@
+import * as Option from 'effect/Option';
+
+import * as TestUtils from '@parischap/configs/TestUtils';
+import * as CVRounderParams from '@parischap/conversions/CVRounderParams';
+import * as CVRoundingOption from '@parischap/conversions/CVRoundingOption';
+
+import { describe, it } from 'vitest';
+
+describe('CVRounderParams', () => {
+  const rounderParams = CVRounderParams.make({
+    precision: 3,
+    roundingOption: CVRoundingOption.Type.HalfEven,
+  });
+  describe('Tag, .toString()', () => {
+    it('moduleTag', () => {
+      TestUtils.assertEquals(
+        Option.some(CVRounderParams.moduleTag),
+        TestUtils.moduleTagFromTestFilePath(import.meta.filename),
+      );
+    });
+
+    it('.toString()', () => {
+      TestUtils.strictEqual(rounderParams.toString(), 'HalfEvenRounderWith3Precision');
+    });
+  });
+});
